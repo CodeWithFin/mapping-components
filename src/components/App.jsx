446a172ef1fcaedb73fa8d06021611746e8ac39f
Finley <finley.mwachia12@gmail.com>
2024-08-19 03:23:21 +0300

    map data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+  
+function createCard(contact){
+  return <Card key = {contact.id}
+  name = {contact.name} 
+  img= {contact.imgURL}
+  tel ={contact.phone}
+  email = {contact.email}/>
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
+{contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -24,7 +32,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
